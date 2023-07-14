b0e6689e47cd6048c47470a7d77052044aa3c701
hicach <hichamachahboun.study@gmail.com>
2023-07-14 18:16:06 +0100

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is ahead of 'origin/main' by 1 commit.
      (use "git push" to publish your local commits)
    
    Changes to be committed:
    	modified:   app/__pycache__/models.cpython-311.pyc
    	new file:   app/migrations/0004_visit_platform.py
    	new file:   app/migrations/__pycache__/0004_visit_platform.cpython-311.pyc
    	modified:   app/models.py

--- app/models.py
+++ app/models.py
@@ -417,6 +417,7 @@ class Visit(models.Model):
     serie = models.ForeignKey('Serie', related_name='visits', on_delete=models.CASCADE, null=True, blank=True)
     season = models.ForeignKey('Season', related_name='visits', on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey('User', related_name='visits', on_delete=models.CASCADE, null=True, blank=True)
+    platform = models.ForeignKey('Platform', related_name='visits', on_delete=models.CASCADE, null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True,null=True)
 class Like(models.Model):
     positive = models.BooleanField(default=True)
